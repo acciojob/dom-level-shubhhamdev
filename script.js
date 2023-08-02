2fb9b58ba01e5f962d72f9375fdc52f9907539f6
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-08-02 20:02:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let element = document.getElementById("level");
+let list = document.querySelectorAll("ul li");
+let level = Array.from(list).indexOf(element)+1;
+
+alert(level);
